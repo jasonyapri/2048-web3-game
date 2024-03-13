0287e90f80d3bf991435eca87efa5ccc14b9bb55
Jason Yapri <jason.yapri@gmail.com>
2024-03-13 16:06:10 +0700

    Upgraded the Prize Distribution Logic and Added placeNewTiles Function

--- src/Web3Game2048.sol
+++ src/Web3Game2048.sol
@@ -2,7 +2,7 @@
 // @author: Jason Yapri
 // @website: https://jasonyapri.com
 // @linkedIn: https://linkedin.com/in/jasonyapri
-// @version: 0.1.1 2024.03.12
+// @version: 0.2.0 2024.03.13
 // Contract: Web3 Game - 2048
 pragma solidity ^0.8.24;
 
@@ -11,9 +11,15 @@ contract Web3Game2048 {
     uint256 public moveCount; // 32 bytes | slot 2
     address public owner; // 20 bytes | slot 3
     uint16[4][4] public gameBoard; // 2 bytes | slot 3
+    bool firstPrizeDistributed; // 1 byte | slot 3
+    bool secondPrizeDistributed; // 1 byte | slot 3
+    bool thirdPrizeDistributed; // 1 byte | slot 3
 
-    uint256 public constant prizePercentage = 50; // 50% of the prize pool amount;
-    uint8 public constant commissionPercentage = 10; // 10% commission of each transfer
+    uint8 public constant GRAND_PRIZE_PERCENTAGE = 50; // 50% of the prize pool amount when reached 2048
+    uint8 public constant FIRST_PRIZE_PERCENTAGE = 10; // 10% of the prize pool amount when reached 1024
+    uint8 public constant SECOND_PRIZE_PERCENTAGE = 5; // 5% of the prize pool amount when reached 512
+    uint8 public constant THIRD_PRIZE_PERCENTAGE = 3; // 3% of the prize pool amount when reached 256
+    uint8 public constant COMMISSION_PERCENTAGE = 10; // 10% commission on every prize distribution
     string public constant AUTHOR_NAME = "Jason Yapri";
     string public constant AUTHOR_WEBSITE = "https://jasonyapri.com";
     string public constant AUTHOR_LINKEDIN =
@@ -52,25 +58,74 @@ contract Web3Game2048 {
         // Set the initial prize pool amount from the amount received during deployment
         owner = msg.sender;
         prizePool = msg.value;
-        placeNewTile();
+        placeTwoNewTiles();
     }
 
     function resetGame() internal {
-        resetTiles();
-        placeNewTile();
-    }
-
-    function resetTiles() internal {
-        // Initialize the game board with empty tiles (0 value)
+        // Reset Tiles
         for (uint8 i = 0; i < 4; i++) {
             for (uint8 j = 0; j < 4; j++) {
                 gameBoard[i][j] = 0;
             }
         }
-
         moveCount = 0;
-
         emit TilesReset();
+
+        placeTwoNewTiles();
+
+        firstPrizeDistributed = false;
+        secondPrizeDistributed = false;
+        thirdPrizeDistributed = false;
+    }
+
+    function placeTwoNewTiles() internal returns (bool) {
+        TileLocation[] memory emptyTiles = new TileLocation[](0);
+
+        for (uint8 i = 0; i < 4; i++) {
+            for (uint8 j = 0; j < 4; j++) {
+                if (gameBoard[i][j] == 0)
+                    emptyTiles[emptyTiles.length] = TileLocation(i, j);
+            }
+        }
+
+        if (emptyTiles.length == 0) return false;
+
+        // Add Tile 2 to two random empty tiles
+        uint256 randomIndex1 = uint256(
+            keccak256(
+                abi.encodePacked(
+                    block.timestamp,
+                    block.prevrandao,
+                    "randomIndex1"
+                )
+            )
+        ) % emptyTiles.length;
+        uint256 randomIndex2;
+        uint8 counter = 0;
+        do {
+            randomIndex2 =
+                uint256(
+                    keccak256(
+                        abi.encodePacked(
+                            block.timestamp,
+                            block.prevrandao,
+                            "randomIndex2",
+                            counter
+                        )
+                    )
+                ) %
+                emptyTiles.length;
+            counter++;
+        } while (randomIndex1 == randomIndex2 && counter < emptyTiles.length);
+
+        uint8 randomRow1 = emptyTiles[randomIndex1].row;
+        uint8 randomCol1 = emptyTiles[randomIndex1].column;
+        uint8 randomRow2 = emptyTiles[randomIndex2].row;
+        uint8 randomCol2 = emptyTiles[randomIndex2].column;
+        gameBoard[randomRow1][randomCol1] = 2;
+        gameBoard[randomRow2][randomCol2] = 2;
+
+        return true;
     }
 
     function placeNewTile() internal returns (bool) {
@@ -235,9 +290,12 @@ contract Web3Game2048 {
         moveCount++;
         emit Moved({player: msg.sender, move: move});
 
-        if (won()) {
-            distributePrize(msg.sender);
+        int8 prizeWon = checkIfPlayerWonPrize();
+        if (prizeWon == 0) {
+            distributePrize(msg.sender, prizeWon);
             resetGame();
+        } else if (prizeWon > 0) {
+            distributePrize(msg.sender, prizeWon);
         } else {
             placeNewTile();
             if (!checkForValidMove()) resetGame();
@@ -263,21 +321,30 @@ contract Web3Game2048 {
         if (!success) revert TransferFailed(owner, msg.value);
     }
 
-    function won() internal view returns (bool) {
-        // Check if a player has reached the winning condition (2048 tile) only after making a move
-        bool hasWon = false;
+    function checkIfPlayerWonPrize() internal returns (int8) {
+        // Check if a player has reached certain tile for the first time
+        // Winning Prize Conditions:
+        // 0 -> 2048
+        // 1 -> 1024
+        // 2 -> 512
+        // 3 -> 256
         for (uint8 i = 0; i < 4; i++) {
             for (uint8 j = 0; j < 4; j++) {
                 if (gameBoard[i][j] == 2048) {
-                    hasWon = true;
-                    break;
+                    return 0;
+                } else if (gameBoard[i][j] == 1024 && !firstPrizeDistributed) {
+                    firstPrizeDistributed = true;
+                    return 1;
+                } else if (gameBoard[i][j] == 512 && !secondPrizeDistributed) {
+                    secondPrizeDistributed = true;
+                    return 2;
+                } else if (gameBoard[i][j] == 256 && !thirdPrizeDistributed) {
+                    thirdPrizeDistributed = true;
+                    return 3;
                 }
             }
-            if (hasWon) {
-                break;
-            }
         }
-        return hasWon;
+        return -1;
     }
 
     receive() external payable {}
@@ -291,10 +358,21 @@ contract Web3Game2048 {
         }
     }
 
-    function distributePrize(address winner) internal {
-        // Transfer half of the prize pool amount to the winner's wallet address
+    function distributePrize(address winner, int8 prizeWon) internal {
+        uint8 prizePercentage;
+        if (prizeWon == 0) {
+            prizePercentage = GRAND_PRIZE_PERCENTAGE;
+        } else if (prizeWon == 1) {
+            prizePercentage = FIRST_PRIZE_PERCENTAGE;
+        } else if (prizeWon == 2) {
+            prizePercentage = SECOND_PRIZE_PERCENTAGE;
+        } else if (prizeWon == 3) {
+            prizePercentage = THIRD_PRIZE_PERCENTAGE;
+        }
+
+        // Transfer certain percentage of the prize pool amount to the winner's wallet address
         uint256 totalPrize = (prizePool * prizePercentage) / 100;
-        uint256 commission = (totalPrize * commissionPercentage) / 100;
+        uint256 commission = (totalPrize * COMMISSION_PERCENTAGE) / 100;
         uint256 winnerPrize = totalPrize - commission;
 
         prizePool -= totalPrize;
