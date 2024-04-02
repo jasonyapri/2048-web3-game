3d42107bdf1190235f02c7794066c2d24a606c41
Jason Yapri <jason.yapri@gmail.com>
2024-04-02 21:31:18 +0700

    Deployment Script to Optimism Sepolia incl. required .env variables and foundry.toml configurations

--- script/Web3Game2048.s.sol
+++ script/Web3Game2048.s.sol
@@ -2,16 +2,24 @@
 // @author: Jason Yapri
 // @website: https://jasonyapri.com
 // @linkedIn: https://linkedin.com/in/jasonyapri
-// @version: 0.1.1 2024.03.12
 // Contract: Web3 Game - 2048
 pragma solidity ^0.8.24;
 
 import {Script, console} from "forge-std/Script.sol";
+import {Web3Game2048} from "../src/Web3Game2048.sol";
 
 contract Web3Game2048Script is Script {
-    function setUp() public {}
+    Web3Game2048 web3Game;
+
+    function setUp() public {
+        vm.createSelectFork(vm.rpcUrl("optimism_sepolia"));
+        // vm.createSelectFork(vm.rpcUrl("optimism_mainnet"));
+    }
 
     function run() public {
-        vm.broadcast();
+        uint256 privateKey = vm.envUint("DEPLOYER_WALLET_PRIVATE_KEY");
+        vm.startBroadcast(privateKey);
+        web3Game = new Web3Game2048(vm.envAddress("OWNER_WALLET_ADDRESS"));
+        vm.stopBroadcast();
     }
 }
